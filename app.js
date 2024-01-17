6319e15080caa018fb06f5b96e03c0a1b8670e91
gxstavo19 <gxstavoferreira@gmail.com>
2024-01-17 13:34:27 -0300

    adição do else
    adição do else no código. "se não";

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ alert("Boas vindas ao jogo do número secreto!");
 let numSecret = 5;
 
 let chute = prompt("escolha um número entre 1 e 10");
-
+// se chute for igual ao número secreto
 if (numSecret == chute) {
   console.log("isso aí! Você descobriu o número secreto (5)");
+} else {
+  alert("você errou :(");
 }
