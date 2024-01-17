4701c08f22c62770cfe11aa8a10f10b11245bb75
gxstavo19 <gxstavoferreira@gmail.com>
2024-01-17 13:25:16 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas ao jogo do número secreto!");
+
+let numSecret = 5;
+
+let chute = prompt("escolha um número entre 1 e 10");
+
+if (numSecret == chute) {
+  console.log("isso aí! Você descobriu o número secreto (5)");
+}
